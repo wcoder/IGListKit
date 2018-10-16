77bfd193a23bea6c32bee17c1619d6c03e476f2c
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-10-16 22:08:26 +0300

    Add empty solution

---
+++ src/IGListKitDemo/MyCollectionViewCell.cs
@@ -0,0 +1,31 @@
+﻿//
+// MyCollectionViewCell.cs
+//
+// Author:
+//       Yauheni Pakala <evgeniy.pakalo@gmail.com>
+//
+// Copyright (c) 2018 Yauheni Pakala
+//
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace IGListKitDemo
+{
+    public partial class MyCollectionViewCell : UICollectionViewCell
+    {
+        public static readonly NSString Key = new NSString("MyCollectionViewCell");
+        public static readonly UINib Nib;
+
+        static MyCollectionViewCell()
+        {
+            Nib = UINib.FromName("MyCollectionViewCell", NSBundle.MainBundle);
+        }
+
+        protected MyCollectionViewCell(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+    }
+}
---
+++ src/IGListKitDemo/ViewController.cs
@@ -0,0 +1,50 @@
+﻿//
+// ViewController.cs
+//
+// Author:
+//       Yauheni Pakala <evgeniy.pakalo@gmail.com>
+//
+// Copyright (c) 2018 Yauheni Pakala
+//
+using System;
+using CoreGraphics;
+using IGListKit;
+using ObjCRuntime;
+using UIKit;
+
+namespace IGListKitDemo
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+        }
+    }
+
+    // TODO:
+    public class LabelSectionController : IGListSectionController
+    {
+        public override CGSize SizeForItemAtIndex(nint index)
+        {
+            return new CGSize((nfloat)CollectionContext?.ContainerSize.Width, 55f);
+        }
+
+        public override UICollectionViewCell CellForItemAtIndex(nint index)
+        {
+            var cell = CollectionContext?.DequeueReusableCellOfClass(new Class(typeof(MyCollectionViewCell)), this, index);
+            return cell;
+        }
+    }
+}
